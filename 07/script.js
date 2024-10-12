95fe4de9206d772e87e23da3e4f91b69f1d1945b
surajc-15 <surajchanaveeragoudra@gmail.com>
2024-10-12 23:33:34 +0530

    lecture 7 notes

--- 07/script.js
+++ 07/script.js
@@ -3,7 +3,7 @@
 // console.log(React);*/
 
 // import ReactDOM from "react-dom";
-
+import React from "react";
 import { createRoot } from "react-dom/client";
 console.log("surafdnksl");
 
@@ -23,7 +23,7 @@ const h1 = <h1>Hello worldsd exploring hot reloading </h1>;
 //   </div>
 // );
 
-function cards(props) {
+function Cards(props) {
   const { key, title, image, brand, price } = props;
   return (
     <div className="card" key={key}>
@@ -44,6 +44,8 @@ function cards(props) {
 // const container = [card, card, card, card, card, card, card, card, card];
 // const maincontainer = <div className="main">{container}</div>;
 const root = createRoot(document.getElementById("root"));
+
+/*
 //use fetch to get data from public api
 console.log("extracting data from api");
 fetch("https://dummyjson.com/products")
@@ -64,6 +66,65 @@ fetch("https://dummyjson.com/products")
       </div>
     );
   });
+
+*/
+
+/*
+//rendering using react elemnent
+root.render(
+  React.createElement(cards, {
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjAMLedZRk2kc2Gg7NC0jRaHIjxa1-vf-b_A&s",
+    key: 1,
+    price: 1000,
+    brand: "Meghanas",
+    title: "food",
+  })
+);
+*/
+
+//First letter should be capital for type
+root.render(
+  <Cards
+    key="1"
+    price="1000"
+    brand="Meghanas"
+    title="food"
+    image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjAMLedZRk2kc2Gg7NC0jRaHIjxa1-vf-b_A&s"
+  />
+);
+
+console.log(
+  <Cards
+    key="1"
+    price="1000"
+    brand="Meghanas"
+    title="food"
+    image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjAMLedZRk2kc2Gg7NC0jRaHIjxa1-vf-b_A&s"
+  />
+);
+
+/*
+fetch("https://dummyjson.com/products")
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+    root.render(
+      React.createElement("div", { className: "container" }, [
+        data.products.map((product) => {
+          return React.createElement(cards, {
+            image: product.thumbnail,
+            key: product.id,
+            title: product.title,
+            brand: product.brand,
+            price: product.price,
+          });
+        }),
+      ])
+    );
+  });
+
+  */
 if (module.hot) {
   module.hot.accept();
 }
