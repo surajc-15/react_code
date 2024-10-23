874104669cd9bf2a7260d2d7e750449b085f5189
surajc-15 <surajchanaveeragoudra@gmail.com>
2024-10-23 10:55:35 +0530

    react useeffect

--- 09_react_states/components/Counter.js
+++ 09_react_states/components/Counter.js
@@ -1,13 +1,18 @@
 import React, { useState } from "react";
 
+import styles from "./counter.module.css";
+
+console.log(styles);
 export default function Counter() {
   //const myStateArray = useState(0);
   const [count, setCount] = useState(0);
   console.log("rendering");
+
   return (
     <div style={{ textAlign: "center" }}>
       <h1>{count}</h1>
       <button
+        className={styles.button}
         onClick={() => {
           setCount(count + 10); //setcount is like updating function
           console.log(count);
